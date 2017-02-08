7e3a6ae45940b3bb876df142ad52f033f01aadf2
Casey Marshall <casey.s.marshall@gmail.com>
2017-02-08 13:55:37 -0800

    Sigh.

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [aleph "0.4.2-alpha12"]])
+                 [aleph "0.4.2-alpha12"]]
+  :deploy-repositories ["release" {:url "https://clojars.org/repo"}])
