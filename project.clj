1b25ff4edc9e783934923bc2d1eff2ea1d0f678f
Casey Marshall <casey.s.marshall@gmail.com>
2017-02-08 13:50:10 -0800

    Set URL.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.github.csm/aleph-haproxy-tcp "0.1.0"
   :description "Aleph wrapper for HAPROXY protocol"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/csm/aleph-haproxy-tcp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
