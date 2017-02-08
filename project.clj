0b43091cfad92e201ad598d4f97ffca1c1482a16
Casey Marshall <casey.s.marshall@gmail.com>
2017-02-08 13:57:28 -0800

    FUUUUU

--- project.clj
+++ project.clj
@@ -1,8 +1,16 @@
-(defproject com.github.csm/aleph-haproxy-tcp "0.1.0"
+(defproject com.github.csm/aleph-haproxy-tcp "0.1.0-SNAPSHOT"
   :description "Aleph wrapper for HAPROXY protocol"
   :url "https://github.com/csm/aleph-haproxy-tcp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [aleph "0.4.2-alpha12"]]
-  :deploy-repositories [["release" {:url "https://clojars.org/repo"}]])
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version"
+                   "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
